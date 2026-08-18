b8446d6f81919c775253753f998fefc7f698e43e
nanomi657-hash <nanomi657@gmail.com>
2026-08-18 17:27:06 +0700

    Add files via upload

--- src/api/anime.js
+++ src/api/anime.js
@@ -1,1 +1,17 @@
+import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_BASE,
+});
+
+export const getHomeAnime = (page = 1) => api.get(`/animasu/home?page=${page}`);
+export const getPopularAnime = (page = 1) => api.get(`/animasu/popular?page=${page}`);
+export const getMoviesAnime = (page = 1) => api.get(`/animasu/movies?page=${page}`);
+export const getOngoingAnime = (page = 1) => api.get(`/animasu/ongoing?page=${page}`);
+export const getLatestAnime = (page = 1) => api.get(`/animasu/latest?page=${page}`);
+export const searchAnime = (query, page = 1) => api.get(`/animasu/search?query=${query}&page=${page}`);
+export const getGenres = () => api.get(`/animasu/genres`);
+export const getAnimeDetail = (slug) => api.get(`/animasu/detail/${slug}`);
+export const getEpisodeDetail = (slug) => api.get(`/animasu/episode/${slug}`);
